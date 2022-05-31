eee21c90fd2b0146ada7c7b89f50e656cab1a744
Daniel Ferraz <Daniel0ferraz@outlook.com>
2022-05-30 23:02:38 -0300

    Props InputFiel esconder icones

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { TextInputProps } from 'react-native';
-
+import { TextInputProps, View } from 'react-native';
+import IconEye from '../../assets/icon-eye_white.svg';
+import IconEyeOff from '../../assets/icon-eye_off.svg';
+import IconCopy from '../../assets/icon-copy_white.svg';
 import * as Styled from './styles';
 
-export default function InputField(props: TextInputProps) {
-  return <Styled.InputField placeholderTextColor={'#A5D8FF'} {...props} />;
+type InputFieldsProps = TextInputProps & {
+  secureTextEntry?: boolean;
+  iconCopyPast?: boolean | undefined;
+  iconSec?: boolean | undefined;
+};
+
+export default function InputField({
+  secureTextEntry,
+  iconCopyPast,
+  iconSec,
+  ...rest
+}: InputFieldsProps) {
+  const [sec, setSec] = useState(secureTextEntry);
+  return (
+    <>
+      <View>
+        <Styled.InputField
+          underlineColorAndroid="trasparent"
+          secureTextEntry={sec}
+          placeholderTextColor={'#ffffff'}
+          iconCopyPast={iconCopyPast}
+          iconSec={iconSec}
+          {...rest}
+        />
+
+        {iconSec ? (
+          <Styled.IconButton onPress={() => setSec(!sec)}>
+            {sec ? (
+              <IconEye fill="#F8F9FA" width={21} height={21} />
+            ) : (
+              <IconEyeOff fill="#F8F9FA" width={21} height={21} />
+            )}
+          </Styled.IconButton>
+        ) : null}
+
+        {iconCopyPast ? (
+          <Styled.IconButton2>
+            <IconCopy width={21} height={21} color="#fff" />
+          </Styled.IconButton2>
+        ) : null}
+      </View>
+    </>
+  );
 }
--- src/components/InputField/styles.ts
+++ src/components/InputField/styles.ts
@@ -11,6 +11,20 @@ export const InputField = styled(TextInput)`
     padding: 10px 56px 9px 16px;
     font-size: 14px;
     font-family: ${theme.fonts.Lexend400};
-    color: ${theme.colors.blue100};
+    color: ${theme.colors.white};
   `}
+`;
+
+export const IconButton = styled.TouchableOpacity`
+  margin-right: 10px;
+  position: absolute;
+  right: 40px;
+  top: 14px;
+`;
+
+export const IconButton2 = styled.TouchableOpacity`
+  margin-right: 10px;
+  position: absolute;
+  right: 8px;
+  top: 14px;
 `;
